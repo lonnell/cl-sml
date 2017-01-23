330c7085c07009cb7c428835b198e9f1bbdad115
Chun Tian (binghe) <binghe.lisp@gmail.com>
2017-01-23 19:08:25 +0100

    imported all lisp sources from Debian's hol88 package

---
+++ lisp/f-ol-syntax.h
@@ -0,0 +1,367 @@
+
+static void L1();
+static void L2();
+static void L3();
+static void L4();
+static void L5();
+static void L6();
+static void L7();
+static void L8();
+static void L9();
+static void L10();
+static void L11();
+static void L12();
+static void L13();
+static void L14();
+static void L15();
+static void L16();
+static void L17();
+static void L18();
+static void L19();
+static void L20();
+static void L21();
+static void L22();
+static void L23();
+static void L24();
+static void L25();
+static void L26();
+static void L27();
+static void L28();
+static void L29();
+static void L30();
+static void L31();
+static void L32();
+static void L33();
+static void L34();
+static void L35();
+static void L36();
+static void L37();
+static void L38();
+static void L39();
+static void L40();
+static void L41();
+static void L42();
+static void L43();
+static void L44();
+static void L45();
+static void L46();
+static void L47();
+static void L48();
+static void L49();
+static void L50();
+static void L51();
+static void L52();
+static void L53();
+static void L54();
+static void L55();
+static void L56();
+static void L57();
+static void L58();
+static void L59();
+static void L60();
+static void L61();
+static void L62();
+static void L63();
+static void L64();
+static void L65();
+static void L66();
+static void L67();
+static void L68();
+static void L69();
+static void L70();
+#define VC1
+#define VC2
+#define VC3
+#define VC4
+#define VC5
+#define VC6
+#define VC7
+#define VC8
+#define VC9
+#define VC10
+#define VC11
+#define VC12
+#define VC13
+#define VC14
+#define VC15
+#define VC16
+#define VC17
+#define VC18
+#define VC19
+#define VC20
+#define VC21
+#define VC22
+#define VC23 object  V37 ,V36;
+#define VC24 object  V43 ,V42;
+#define VC25 object  V46;
+#define VC26
+#define VC27
+#define VC28 object  V57 ,V56;
+#define VC29
+#define VC30
+#define VC31
+#define VC32 object  V62;
+#define VC33 object  V68 ,V67;
+#define VC34
+#define VC35
+#define VC36
+#define VC37 object  V79 ,V78 ,V77 ,V76;
+#define VC38
+#define VC39 object  V82;
+#define VC40 object  V96 ,V95 ,V94 ,V93 ,V92 ,V91 ,V90 ,V89 ,V88 ,V87 ,V86 ,V85;
+#define VC41
+#define VC42 object  V112 ,V111 ,V110 ,V109 ,V108 ,V107 ,V106 ,V105 ,V104 ,V103 ,V102 ,V101;
+#define VC43 object  V117 ,V116;
+#define VC44
+#define VC45 object  V131 ,V130 ,V129 ,V128;
+#define VC46 object  V141 ,V140 ,V139 ,V136;
+#define VC47 object  V148 ,V147 ,V145;
+#define VC48
+#define VC49
+#define VC50
+#define VC51 object  V160 ,V159;
+#define VC52 object  V169 ,V168;
+#define VC53
+#define VC54 object  V173;
+#define VC55
+#define VC56
+#define VC57
+#define VC58 object  V185 ,V184;
+#define VC59
+#define VC60
+#define VC61 object  V192 ,V190;
+#define VC62
+#define VC63
+#define VC64 object  V197;
+#define VC65
+#define VC66
+#define VC67
+#define VC68
+#define VC69
+#define VC70
+#define VM70 2
+#define VM69 2
+#define VM68 2
+#define VM67 2
+#define VM66 1
+#define VM65 2
+#define VM64 2
+#define VM63 2
+#define VM62 2
+#define VM61 3
+#define VM60 2
+#define VM59 2
+#define VM58 2
+#define VM57 7
+#define VM56 5
+#define VM55 5
+#define VM54 2
+#define VM53 2
+#define VM52 6
+#define VM51 5
+#define VM50 5
+#define VM49 2
+#define VM48 4
+#define VM47 3
+#define VM46 3
+#define VM45 2
+#define VM44 3
+#define VM43 3
+#define VM42 2
+#define VM41 4
+#define VM40 2
+#define VM39 2
+#define VM38 2
+#define VM37 3
+#define VM36 4
+#define VM35 2
+#define VM34 2
+#define VM33 6
+#define VM32 3
+#define VM31 2
+#define VM30 2
+#define VM29 4
+#define VM28 5
+#define VM27 2
+#define VM26 2
+#define VM25 3
+#define VM24 6
+#define VM23 6
+#define VM22 2
+#define VM21 2
+#define VM20 3
+#define VM19 4
+#define VM18 2
+#define VM17 2
+#define VM16 3
+#define VM15 4
+#define VM14 2
+#define VM13 2
+#define VM12 2
+#define VM11 3
+#define VM10 2
+#define VM9 2
+#define VM8 3
+#define VM7 2
+#define VM6 2
+#define VM5 3
+#define VM4 2
+#define VM3 2
+#define VM2 5
+#define VM1 2
+static void * VVi[109]={
+#define Cdata VV[108]
+(void *)(L1),
+(void *)(L2),
+(void *)(L3),
+(void *)(L4),
+(void *)(L5),
+(void *)(L6),
+(void *)(L7),
+(void *)(L8),
+(void *)(L9),
+(void *)(L10),
+(void *)(L11),
+(void *)(L12),
+(void *)(L13),
+(void *)(L14),
+(void *)(L15),
+(void *)(L16),
+(void *)(L17),
+(void *)(L18),
+(void *)(L19),
+(void *)(L20),
+(void *)(L21),
+(void *)(L22),
+(void *)(L23),
+(void *)(L24),
+(void *)(L25),
+(void *)(L26),
+(void *)(L27),
+(void *)(L28),
+(void *)(L29),
+(void *)(L30),
+(void *)(L31),
+(void *)(L32),
+(void *)(L33),
+(void *)(L34),
+(void *)(L35),
+(void *)(L36),
+(void *)(L37),
+(void *)(L38),
+(void *)(L39),
+(void *)(L40),
+(void *)(L41),
+(void *)(L42),
+(void *)(L43),
+(void *)(L44),
+(void *)(L45),
+(void *)(L46),
+(void *)(L47),
+(void *)(L48),
+(void *)(L49),
+(void *)(L50),
+(void *)(L51),
+(void *)(L52),
+(void *)(L53),
+(void *)(L54),
+(void *)(L55),
+(void *)(L56),
+(void *)(L57),
+(void *)(L58),
+(void *)(L59),
+(void *)(L60),
+(void *)(L61),
+(void *)(L62),
+(void *)(L63),
+(void *)(L64),
+(void *)(L65),
+(void *)(L66),
+(void *)(L67),
+(void *)(L68),
+(void *)(L69),
+(void *)(L70)
+};
+#define VV (VVi)
+static void LnkT107();
+static void (*Lnk107)() = LnkT107;
+static void LnkT106();
+static void (*Lnk106)() = LnkT106;
+static void LnkT105();
+static void (*Lnk105)() = LnkT105;
+static void LnkT104();
+static void (*Lnk104)() = LnkT104;
+static object  LnkTLI103(object,...);
+static object  (*LnkLI103)() = (object (*)()) LnkTLI103;
+static object  LnkTLI102(object,...);
+static object  (*LnkLI102)() = (object (*)()) LnkTLI102;
+static void LnkT101();
+static void (*Lnk101)() = LnkT101;
+static void LnkT100();
+static void (*Lnk100)() = LnkT100;
+static void LnkT99();
+static void (*Lnk99)() = LnkT99;
+static void LnkT98();
+static void (*Lnk98)() = LnkT98;
+static void LnkT97();
+static void (*Lnk97)() = LnkT97;
+static void LnkT96();
+static void (*Lnk96)() = LnkT96;
+static void LnkT95();
+static void (*Lnk95)() = LnkT95;
+static void LnkT94();
+static void (*Lnk94)() = LnkT94;
+static void LnkT93();
+static void (*Lnk93)() = LnkT93;
+static void LnkT92();
+static void (*Lnk92)() = LnkT92;
+static void LnkT91();
+static void (*Lnk91)() = LnkT91;
+static void LnkT90();
+static void (*Lnk90)() = LnkT90;
+static void LnkT89();
+static void (*Lnk89)() = LnkT89;
+static void LnkT88();
+static void (*Lnk88)() = LnkT88;
+static void LnkT87();
+static void (*Lnk87)() = LnkT87;
+static void LnkT86();
+static void (*Lnk86)() = LnkT86;
+static void LnkT85();
+static void (*Lnk85)() = LnkT85;
+static void LnkT84();
+static void (*Lnk84)() = LnkT84;
+static void LnkT83();
+static void (*Lnk83)() = LnkT83;
+static void LnkT81();
+static void (*Lnk81)() = LnkT81;
+static void LnkT80();
+static void (*Lnk80)() = LnkT80;
+static void LnkT79();
+static void (*Lnk79)() = LnkT79;
+static void LnkT78();
+static void (*Lnk78)() = LnkT78;
+static void LnkT77();
+static void (*Lnk77)() = LnkT77;
+static void LnkT76();
+static void (*Lnk76)() = LnkT76;
+static void LnkT75();
+static void (*Lnk75)() = LnkT75;
+static void LnkT74();
+static void (*Lnk74)() = LnkT74;
+static void LnkT73();
+static void (*Lnk73)() = LnkT73;
+static void LnkT72();
+static void (*Lnk72)() = LnkT72;
+static void LnkT71();
+static void (*Lnk71)() = LnkT71;
+static void LnkT70();
+static void (*Lnk70)() = LnkT70;
+static void LnkT69();
+static void (*Lnk69)() = LnkT69;
+static void LnkT68();
+static void (*Lnk68)() = LnkT68;
+static void LnkT67();
+static void (*Lnk67)() = LnkT67;
+static void LnkT66();
+static void (*Lnk66)() = LnkT66;
